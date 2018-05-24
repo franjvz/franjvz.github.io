92b49f2e83369d513cee55ca745e324d3992042a
Francisco Javier Mora Muñoz <franjaviez@gmail.com>
2018-05-24 15:37:57 +0200

    Optimizaciones y mejoras responsive

--- js/resume.js
+++ js/resume.js
@@ -39,39 +39,42 @@
       );
     });
 
-    // Reutilizando el mismo modal para los items
-    $('#proyectos .card-img-wrapper').on('click', function(){
-      var item = $(this);
-      var proyecto = item.parent();
+    // Si la pantalla es mas grande vincular evento click para agrandar fotos
+    if ($(window).width() > 767) {
+      $('#proyectos .card-img-wrapper').on('click', function(){
+        var item = $(this);
+        var proyecto = item.parent();
 
-      // Obtener titulo item
-      var tituloitem = proyecto.find('.card-header').html();
+        // Obtener titulo item
+        var tituloitem = proyecto.find('.card-header').html();
 
-      // Obtener imagen item
-      var imagenitem = item.find('.card-img-top').attr("src").replace("350px", "720px");
+        // Obtener imagen item
+        var imagenitem = item.find('.card-img-top').attr("src").replace("350px", "720px");
 
-      // Obtener tecnologías
-      var tecnologias = proyecto.find('.card-footer').html();
+        // Obtener tecnologías
+        var tecnologias = proyecto.find('.card-footer').html();
 
-      // Migrar datos a diálogo
-      $('#miDialogo .modal-title').html(tituloitem);
-      var imagen = '<img class="img img-modal" src="'+imagenitem+'" />';
-      $('#miDialogo .modal-body').html(imagen);
-      $('#miDialogo .modal-footer').html(tecnologias);
-
-    })
+        // Reutilizando el mismo modal para los items
+        $('#miDialogo .modal-title').html(tituloitem);
+        var imagen = '<img class="img img-modal" src="'+imagenitem+'" />';
+        $('#miDialogo .modal-body').html(imagen);
+        $('#miDialogo .modal-footer').html(tecnologias);
+      })
+    }else{
+      $("#miDialogo").remove();
+    }
   });
 
-
+// Encriptar email para no spam
 function nospamporfavor(){ var
-dom='&#x67;&#x6D;&#x61;&#x69;&#x6C;.&#x63;&#x6F;&#x6D;',
-display='&#x4D;&#x69; &#x63;&#x6F;&#x72;&#x72;&#x65;&#x6F;',
-x40='&#x40;',
-part1='&#x6D;&#x61;&#x69;&#x6C;&#x74;&#x6F;:',
-name='&#x66;&#x72;&#x61;&#x6E;&#x6A;&#x61;&#x76;&#x69;&#x65;&#x7A;',
-stuff='';
+  dom='&#x67;&#x6D;&#x61;&#x69;&#x6C;.&#x63;&#x6F;&#x6D;',
+  display='&#x4D;&#x69; &#x63;&#x6F;&#x72;&#x72;&#x65;&#x6F;',
+  x40='&#x40;',
+  part1='&#x6D;&#x61;&#x69;&#x6C;&#x74;&#x6F;:',
+  name='&#x66;&#x72;&#x61;&#x6E;&#x6A;&#x61;&#x76;&#x69;&#x65;&#x7A;',
+  stuff='';
 
-document.getElementById('nospam').innerHTML = '<a title="'+display+'" href="'+part1+name+x40+dom+stuff+'">'+display+'<\/a>';
+  document.getElementById('nospam').innerHTML = '<a title="'+display+'" href="'+part1+name+x40+dom+stuff+'">'+display+'<\/a>';
 }
 
 // Formatea una fecha para que salga bonita  
